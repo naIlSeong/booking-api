8c2d3086bff7297152ac85c8997fa1411a8723b3
nailseong <nnis9857@gmail.com>
2020-12-17 14:30:26 +0900

    Setup User E2E Test

--- src/app.module.ts
+++ src/app.module.ts
@@ -41,7 +41,8 @@ import { Team } from './team/entity/team.entity';
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
       synchronize: true,
-      logging: true,
+      logging:
+        process.env.NODE_ENV !== 'prod' && process.env.NODE_ENV !== 'test',
       entities: [User, Booking, Place, PlaceLocation, Team],
     }),
     GraphQLModule.forRoot({
---
+++ test/user.e2e-spec.ts
@@ -0,0 +1,29 @@
+import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppModule } from 'src/app.module';
+import { getConnection } from 'typeorm';
+
+describe('UserModule (e2e)', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = module.createNestApplication();
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await getConnection().dropDatabase();
+    await app.close();
+  });
+
+  it.todo('createUser');
+  it.todo('login');
+  it.todo('me');
+  it.todo('editUser');
+  it.todo('getUser');
+  it.todo('deleteUser');
+});
