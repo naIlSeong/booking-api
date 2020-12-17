fd0d9fe352d88ae7279f69b3f27b85f29ee03b11
nailseong <nnis9857@gmail.com>
2020-12-17 14:56:47 +0900

    Add createUser Test

--- test/user.e2e-spec.ts
+++ test/user.e2e-spec.ts
@@ -2,9 +2,19 @@ import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppModule } from 'src/app.module';
 import { getConnection } from 'typeorm';
+import * as request from 'supertest';
+
+const user = {
+  username: 'testUserName',
+  password: 'testPassword',
+  studentId: 123456,
+};
 
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
+
+  const publicTest = (query: string) =>
+    request(app.getHttpServer()).post('/graphql').send({ query });
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -20,7 +30,89 @@ describe('UserModule (e2e)', () => {
     await app.close();
   });
 
-  it.todo('createUser');
+  describe('createUser', () => {
+    it('create user', () => {
+      return publicTest(`
+            mutation {
+              createUser(input: {
+                username: "${user.username}"
+                password: "${user.password}"
+                studentId: ${user.studentId}
+              }) {
+                ok
+                error
+              }
+            }
+        `)
+        .expect(200)
+        .expect((res) => {
+          const {
+            body: {
+              data: {
+                createUser: { ok, error },
+              },
+            },
+          } = res;
+          expect(ok).toEqual(true);
+          expect(error).toEqual(null);
+        });
+    });
+
+    it('Error: Already exist username', () => {
+      return publicTest(`
+            mutation {
+              createUser(input: {
+                username: "${user.username}"
+                password: "${user.password}"
+                studentId: ${user.studentId}
+              }) {
+                ok
+                error
+              }
+            }
+        `)
+        .expect(200)
+        .expect((res) => {
+          const {
+            body: {
+              data: {
+                createUser: { ok, error },
+              },
+            },
+          } = res;
+          expect(ok).toEqual(false);
+          expect(error).toEqual('Already exist username');
+        });
+    });
+
+    it('Error: Already exist studentID', () => {
+      return publicTest(`
+            mutation {
+              createUser(input: {
+                username: "otherUsername"
+                password: "${user.password}"
+                studentId: ${user.studentId}
+              }) {
+                ok
+                error
+              }
+            }
+        `)
+        .expect(200)
+        .expect((res) => {
+          const {
+            body: {
+              data: {
+                createUser: { ok, error },
+              },
+            },
+          } = res;
+          expect(ok).toEqual(false);
+          expect(error).toEqual('Already exist studentID');
+        });
+    });
+  });
+
   it.todo('login');
   it.todo('me');
   it.todo('editUser');
